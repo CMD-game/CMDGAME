acfc1b78ad13de4fdff6130385b2d8f6c94372c3
mathking3975 <mathking3975@users.noreply.github.com>
2020-11-26 23:58:14 +0900

    Update maple_shooter.py

--- maple_shooter.py
+++ maple_shooter.py
@@ -1,4 +1,5 @@
 # 게임 조건
+# 0. 더 좋은 조건이 있다면 추가나 수정 바람
 # 1. 캐릭터 : 좌우로 움직일 수 있고 활을 쏨
 # 2. 적은 양쪽에서 다가오고, 방향을 조절하여 맞춰야 함
 # 3. 화살에 적이 맞으면 적이 죽음과 동시에 경험치가 오름
@@ -49,11 +50,10 @@ character_Exp = 0
 MAX_Exp = 100
 invincibility = 0
 
-character_to_x_LEFT = 0
-character_to_x_LEFT_press = 0
+character_to_x_LEFT = 0 
+character_to_x_LEFT_press = 0 # 왼쪽을 보고 있는지 확인하기 위한 변수
 character_to_x_RIGHT = 0
 character_to_x_RIGHT_press = 0
-
 arrow = pygame.image.load("C:/Users/user/Desktop/PythonWorkSpace/CMDGAME/M_arrow_LEFT.png")
 arrow_RIGHT = pygame.image.load("C:/Users/user/Desktop/PythonWorkSpace/CMDGAME/M_arrow_RIGHT.png")
 arrow_size = character.get_rect().size
@@ -61,7 +61,7 @@ arrow_width = character_size[0]
 arrow_height = character_size[1]
 arrow_x_pos = character_x_pos
 arrow_y_pos = character_y_pos + 50
-arrow_LEFT = True
+arrow_LEFT = True 
 arrow_speed = 0.6
 
 enemy_slime = pygame.image.load("C:/Users/user/Desktop/PythonWorkSpace/CMDGAME/M_enemy_slime.png")
@@ -70,12 +70,13 @@ enemy_slime_width = enemy_slime_size[0]
 enemy_slime_height = enemy_slime_size[1]
 enemy_slime_x_pos = 0
 enemy_slime_y_pos = screen_height - enemy_slime_height - 50
-enemy_slime_regen = 0.5
-enemy_slime_regen_time = 0
+enemy_slime_regen = 0.5 # 실제 리젠 시간
+enemy_slime_regen_time = 0 # 리젠 시간을 재기 위한 변수(0으로 고정)
 
 enemy_slime_attack = 5
 
-game_font = pygame.font.Font(None, 40)
+# 필수
+game_font = pygame.font.Font(None, 40) 
 total_time = 0
 start_ticks = pygame.time.get_ticks()
 
@@ -83,11 +84,11 @@ running = True
 Slime = True 
 Shoot = False
 while running:
-    dt = clock.tick(60)
+    dt = clock.tick(60) # 프레임 수
 
     # 2. 이벤트 처리 (키보드, 마우스 등)
     for event in pygame.event.get(): 
-        if event.type == pygame.QUIT: 
+        if event.type == pygame.QUIT: # 창을 닫았을 때
             running = False 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
@@ -98,7 +99,7 @@ while running:
                 character_to_x_RIGHT += character_speed
                 character_to_x_LEFT_press = 0
                 character_to_x_RIGHT_press = 1
-            elif event.key == pygame.K_a:
+            elif event.key == pygame.K_a: # a키를 누름 : 공격
                 if Shoot == False:
                     arrow_x_pos = character_x_pos
                     arrow_y_pos = character_y_pos + 50
@@ -121,19 +122,16 @@ while running:
             arrow_x_pos += arrow_speed * dt
         arrow_y_pos += 1
     else:
-        arrow_y_pos = -1000
+        arrow_y_pos = -1000 # 화살을 제거 (더 나은 방안이 있으면 수정 바람)
 
-    character_x_pos += (character_to_x_LEFT + character_to_x_RIGHT) * dt
+    character_x_pos += (character_to_x_LEFT + character_to_x_RIGHT) * dt # 캐릭터 이동
     if character_x_pos < 0:
         character_x_pos = 0
     if character_x_pos > screen_width - character_width:
         character_x_pos = screen_width - character_width
-
-            
-
-    # 3. 게임 캐릭터 위치 정의
    
     # 4. 충돌 처리 
+    # 필수 (캐릭터 크기를 재는 함수들)
     character_rect = character.get_rect()
     character_rect.left = character_x_pos
     character_rect.top = character_y_pos
@@ -146,7 +144,7 @@ while running:
     arrow_rect.left = arrow_x_pos
     arrow_rect.top = arrow_y_pos
 
-    if arrow_rect.colliderect(enemy_slime_rect):
+    if arrow_rect.colliderect(enemy_slime_rect): # 화살과 슬라임이 충돌
         Slime = False
         Shoot = False
         character_Exp += 10
@@ -162,8 +160,8 @@ while running:
     if Slime == True:
         enemy_slime_x_pos = 0
 
-    if invincibility == 0: 
-        if character_rect.colliderect(enemy_slime_rect):
+    if invincibility == 0: # 무적이 아닐 때
+        if character_rect.colliderect(enemy_slime_rect): # 캐릭터와 슬라임이 충돌
             character_HP -= enemy_slime_attack
             print("HP - " + str(int(enemy_slime_attack)) + ", 남은 HP : " + str(int(character_HP)))
             invincibility = 2
@@ -172,13 +170,14 @@ while running:
                 running = False
 
     elapsed_time = (pygame.time.get_ticks() - start_ticks) / 1000
-    if invincibility == 2:
+    if invincibility == 
         invincibility_time = elapsed_time
         invincibility = 1
     if invincibility == 1:
-        if (pygame.time.get_ticks() - start_ticks) / 1000 - invincibility_time >= 1:
+        if (pygame.time.get_ticks() - start_ticks) / 1000 - invincibility_time >= 1: # 1: 무적 시간(초)
             invincibility = 0
 
+    # 게임 화면 표시
     timer = game_font.render("Time : {}".format(int(elapsed_time)), True, (255, 255, 255))
     HP = game_font.render("HP : {}".format(int(character_HP)), True, (255, 0, 0))
     MP = game_font.render("MP : {}".format(int(character_MP)), True, (0, 0, 255))
@@ -186,7 +185,7 @@ while running:
 
     # 5. 화면에 그리기
     screen.blit(background, (0, 0))
-    if invincibility == True:
+    if invincibility == True: # 무적 시간에 깜빡임 구현 (더 나은 방안이 있으면 수정 바람)
         if ((pygame.time.get_ticks() - start_ticks) / 1000 - invincibility_time) <= 0.25 or (((pygame.time.get_ticks() - start_ticks) / 1000 - invincibility_time) >= 0.5 and ((pygame.time.get_ticks() - start_ticks) / 1000 - invincibility_time) <= 0.75):
             pass
         else:
@@ -214,7 +213,7 @@ while running:
             screen.blit(arrow, (arrow_x_pos, arrow_y_pos))
         else:
             screen.blit(arrow_RIGHT, (arrow_x_pos, arrow_y_pos))
-    screen.blit(timer, (10, 10))
+    screen.blit(timer, (10, 10)) # 필수들
     screen.blit(HP, ((screen_width - 150), 10))
     screen.blit(MP, ((screen_width - 150), 40))
     screen.blit(Exp, ((screen_width - 500), 10))
